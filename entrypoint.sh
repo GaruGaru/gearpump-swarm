c8288ded57a914aad411779d6c9ad39070a492b8
GaruGaru <garuglieritommaso@gmail.com>
2017-10-16 22:26:20 +0200

    fixed entrypoint configuration path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #! /bin/sh -
 
-sed -i -e 's/{{MASTERS}}/'"$MASTERS"'/g' gearpump_2.11-0.8.4-incubating/conf/gear.conf
+CONFIGURATION=gearpump_2.11-0.8.4-incubating/conf/gear.conf
 
-sed -i -e 's/{{HOSTNAME}}/'"$HOSTNAME"'/g' gearpump_2.11-0.8.4-incubating/conf/gear.conf
+sed -i -e 's/{{MASTERS}}/'"$MASTERS"'/g' ${CONFIGURATION}
 
-sed -i -e 's/{{WORKER_SLOTS}}/'"$WORKER_SLOTS"'/g' gearpump_2.11-0.8.4-incubating/conf/gear.conf
+sed -i -e 's/{{HOSTNAME}}/'"$HOSTNAME"'/g' ${CONFIGURATION}
+
+sed -i -e 's/{{WORKER_SLOTS}}/'"$WORKER_SLOTS"'/g' ${CONFIGURATION}
 
